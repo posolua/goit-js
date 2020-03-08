5906dfa7c48f371e28cebdecc31edcc4a1ed11a7
Serhii Poslovskyi <s.poslovskiy@gmail.com>
2020-03-08 22:23:58 +0200

    task 4 hw 7 commit

--- goit-js-hw-07/js/task-04.js
+++ goit-js-hw-07/js/task-04.js
@@ -4,3 +4,13 @@
 // Создай переменную counterValue в которой будет хранится текущее значение счетчика.
 // Создай функции increment и decrement для увеличения и уменьшения значения счетчика
 // Добавь слушатели кликов на кнопки, вызовы функций и обновление интерфейса
+
+const incrementBtn = document.querySelector('button[data-action="increment"]');
+const decrementBtn = document.querySelector('button[data-action="decrement"]');
+let counterValue = document.querySelector('#value');
+
+const increment = () =>
+  (counterValue.textContent = +counterValue.textContent + 1);
+const decrement = () => (counterValue.textContent -= 1);
+incrementBtn.addEventListener('click', increment);
+decrementBtn.addEventListener('click', decrement);
